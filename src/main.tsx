432cca94aeda617990f545c4d60913c533f19f21
Stephen Gregorini de Avo <stephen.gregorini@eximio.med.br>
2025-10-01 01:43:31 -0300

    update de novo

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AuthProvider } from '@/context/AuthContext'
-import { MeProvider } from '@/context/MeContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
-      <MeProvider>
-        <App />   {/* 🚫 sem BrowserRouter aqui */}
-      </MeProvider>
+      <App />   {/* 🚫 BrowserRouter já está dentro do App.tsx */}
     </AuthProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
